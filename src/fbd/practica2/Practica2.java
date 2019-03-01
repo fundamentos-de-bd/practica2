f5d78d416086e80d4ad78efbecc459c857fd9989
isaacn97 <isaacn97@hotmail.com>
2019-02-28 18:33:24 -0600

    Añade una llamada a Datos para generar archivos de prueba al ejecutar el jar resultado de Ant

--- src/fbd/practica2/Practica2.java
+++ src/fbd/practica2/Practica2.java
@@ -1,7 +1,6 @@
 package fbd.practica2;
 
 import java.io.*;
-import org.json.*;
 
 public class Practica2 {
     public static Empleado[] empleados;
@@ -9,7 +8,7 @@ public class Practica2 {
     
     public static void main(String[] a) {
         System.out.println("En construcción");
-        Datos.testJSON();
+        Datos.testCSV();
     }
     
 }
